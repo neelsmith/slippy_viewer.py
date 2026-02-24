9d7a6ff5ee8dab27948314173356425241906c74
Neel Smith <dnsmith.neel@gmail.com>
2026-02-24 17:16:57 -0500

    TIdy nb

--- slippy.py
+++ slippy.py
@@ -1,30 +1,22 @@
 import marimo
 
-__generated_with = "0.19.6"
+__generated_with = "0.20.2"
 app = marimo.App(width="medium")
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     import marimo as mo
+
+    return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    # IIIF Viewer in marimo
+    """)
     return
-
-
-@app.cell
-def _():
-    from pathlib import Path
-    return (Path,)
-
-
-@app.cell
-def _(Path):
-    import anywidget
-    import traitlets
-
-    class IIIFViewer(anywidget.AnyWidget):
-        _esm = Path(__file__).parent / "iiif_viewer.js"
-        url = traitlets.Unicode().tag(sync=True)
-    return (IIIFViewer,)
 
 
 @app.cell
@@ -37,6 +29,38 @@ def _(IIIFViewer):
 def _(viewer):
     viewer
     return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ## Computation
+    """)
+    return
+
+
+@app.cell
+def _():
+    from pathlib import Path
+
+    return (Path,)
+
+
+@app.cell
+def _():
+    import anywidget
+    import traitlets
+
+    return anywidget, traitlets
+
+
+@app.cell
+def _(Path, anywidget, traitlets):
+    class IIIFViewer(anywidget.AnyWidget):
+        _esm = Path(__file__).parent / "iiif_viewer.js"
+        url = traitlets.Unicode().tag(sync=True)
+
+    return (IIIFViewer,)
 
 
 if __name__ == "__main__":
